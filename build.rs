65c838ed0c0e2aa4cae1f3e200d628c544d9a42b
Matty Kuhn <matty.kuhn.1@gmail.com>
2024-12-05 21:25:29 -0600

    simple hello world

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::{fs::File, io::Write, path::PathBuf};
+
+fn main() {
+    let out = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-search={}", out.display());
+
+    let memory_x = include_bytes!("memory.x");
+    let mut f = File::create(out.join("memory.x")).unwrap();
+    f.write_all(memory_x).unwrap();
+    println!("cargo:rerun-if-changed=memory.x");
+
+    let rp235x_riscv_x = include_bytes!("rp235x_riscv.x");
+    let mut f = File::create(out.join("rp235x_riscv.x")).unwrap();
+    f.write_all(rp235x_riscv_x).unwrap();
+    println!("cargo:rerun-if-changed=rp235x_riscv.x");
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+
+#![no_std]
+#![no_main]
+
+use panic_halt;
+
+use rp235x_hal as hal;
+
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[hal::entry]
+fn main() -> ! {
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    let mut watchdog = hal::watchdog::Watchdog::new(pac.WATCHDOG);
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(100);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(100);
+    }
+}
+
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 4] = [
+    hal::binary_info::rp_program_name!(c"hello-world"),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"Hello World"),
+    hal::binary_info::rp_program_build_attribute!(),
+];
